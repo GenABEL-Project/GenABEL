9c1c8d46f8004be87992266a7ec9bfad11f8310b
Yurii Aulchenko <yurii.aulchenko@gmail.com>
2011-04-18 09:12:31 +0000

    update Xia's polygenic_hglm; updated check.marker, added warning if no. Y-markers is < 10
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/genabel/pkg/GenABEL@709 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/polygenic_hglm.R
+++ R/polygenic_hglm.R
@@ -1,9 +1,7 @@
 #' Estimation of polygenic model
 #' 
-#' Estimation of polygenic model using hierarchical
-#' GLM (hglm package)
-#' Tell when REML is used, what otherwise; 
-#' what it is doing, how to do tests, add more examples, references, etc.
+#' Estimation of polygenic model using a hierarchical generalized linear model 
+#' (HGLM; Lee and Nelder 1996. \code{hglm} package; Ronnegard et al. 2010).
 #' 
 #' @param formula Formula describing fixed effects to be used in analysis, e.g. 
 #' y ~ a + b means that outcome (y) depends on two covariates, a and b. 
@@ -12,18 +10,36 @@
 #' @param kinship.matrix Kinship matrix, as provided by e.g. ibs(,weight="freq"), 
 #' or estimated outside of GenABEL from pedigree data.
 #' @param data An (optional) object of \code{\link{gwaa.data-class}} or a data frame with 
-#' outcome and covariates
+#' outcome and covariates.
 #' @param family a description of the error distribution and link function to be 
 #' used in the mean part of the model (see \code{\link{family}} for details of 
-#' family functions)
-#' @param conv 'conv' parameter of 'hglm' (stricter then default)
-#' @param maxit 'maxit' parameter of 'hglm' (stricter then default)
-#' @param ... other parameters passed to 'hglm' call
+#' family functions).
+#' @param conv 'conv' parameter of \code{hglm} (stricter than default, for great precision, use 1e-8).
+#' @param maxit 'maxit' parameter of \code{hglm} (stricter than default).
+#' @param ... other parameters passed to \code{hglm} call
 #' 
 #' @author Xia Shen, Yurii Aulchenko
 #' 
+#' @details
+#'
+#' The algorithm gives extended quasi-likelihood (EQL) estimates
+#' for restricted maximum likelihood (REML) (Ronnegard et al. 2010).
+#' Implemented is the inter-connected GLM interpretation of HGLM (Lee and Nelder 2001).
+#' Testing on fixed and random effects estimates are directly done using inter-connected
+#' GLMs. Testing on dispersion parameters (variance components) can be done by extracting
+#' the profile likelihood function value of REML.
+#' 
 #' @references 
-#' need to put reference here
+#'
+#' Ronnegard, L, Shen, X and Alam, M (2010). hglm: A Package For Fitting 
+#' Hierarchical Generalized Linear Models. \emph{The R Journal}, \bold{2}(2), 20-28.
+#'
+#' Lee, Y and Nelder JA (2001). Hierarchical generalised linear models: 
+#' A synthesis of generalised linear models, random-effect models 
+#' and structured dispersions. \emph{Biometrika}, \bold{88}(4), 987-1006.
+#'
+#' Lee, Y and Nelder JA (1996). Hierarchical Generalized Linear Models. 
+#' \emph{Journal of the Royal Statistical Society. Series B}, \bold{58}(4), 619-678.
 #' 
 #' @seealso 
 #' \code{\link{polygenic}},
@@ -35,22 +51,56 @@
 #' df <- ge03d2ex.clean[,autosomal(ge03d2ex.clean)]
 #' gkin <- ibs(df,w="freq")
 #' 
-#' # for quantitative traits
-#' h2ht <- polygenic_hglm(height ~ sex + age,kin=gkin,df)
-#' # estimate of heritability
+#' # ----- for quantitative traits
+#' h2ht <- polygenic_hglm(height ~ sex + age, kin = gkin, df)
+#' # ----- estimate of heritability
 #' h2ht$esth2
-#' # other parameters
+#' # ----- other parameters
 #' h2ht$h2an
 #' 
-#' # for binary traits
-#' h2dm <- polygenic_hglm(dm2 ~ sex + age,kin=gkin,df,family=binomial(link = 'logit'))
-#' # estimated parameters
+#' # ----- test the significance of fixed effects 
+#' # ----- (e.g. quantitative trait)
+#' h2ht <- polygenic_hglm(height ~ sex + age, kin = gkin, df)
+#' # ----- summary with standard errors and p-values
+#' summary(h2ht$hglm)
+#' # ----- output for the fixed effects part
+#' # ...
+#' # MEAN MODEL
+#' # Summary of the fixed effects estimates 
+#' #              Estimate Std. Error t value Pr(>|t|)    
+#' # (Intercept) 169.53525    2.57624  65.807  < 2e-16 ***
+#' # sex          14.10694    1.41163   9.993  4.8e-15 ***
+#' # age          -0.15332    0.05208  -2.944  0.00441 ** 
+#' # ---
+#' # Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1 
+#' # Note: P-values are based on 69 degrees of freedom
+#' # ...
+#' # ----- extract fixed effects estimates and standard errors
+#' h2ht$h2an
+#' 
+#' # test the significance of polygenic effect
+#' h2ht <- polygenic_hglm(height ~ sex + age, kin = gkin, df, method = 'REML')
+#' nullht <- lm(height ~ sex + age, df)
+#' l1 <- h2ht$ProfLogLik
+#' l0 <- as.numeric(logLik(nullht))
+#' # the likelihood ratio test (LRT) statistic
+#' (S <- -2*(l0 - l1))
+#' # 5% right-tailed significance cutoff 
+#' # for 50:50 mixture distribution between point mass 0 and \chi^{2}(1)
+#' # Self, SG and Liang KY (1987) Journal of the American Statistical Association.
+#' qchisq(((1 - .05) - .50)/.50, 1)
+#' # p-value
+#' pchisq(S, 1, lower.tail = FALSE)*2
+#' 
+#' #' # ----- for binary traits
+#' h2dm <- polygenic_hglm(dm2 ~ sex + age, kin = gkin, df, family = binomial(link = 'logit'))
+#' # ----- estimated parameters
 #' h2dm$h2an
 #' 
 #' 
 #' @keywords htest
 #' 
-"polygenic_hglm" <- function(formula,kinship.matrix,data,family=gaussian(), conv=1e-8, maxit=100, ... )
+"polygenic_hglm" <- function(formula, kinship.matrix, data, family = gaussian(), conv = 1e-6, maxit = 100, ...)
 {
 	if (!require(hglm))
 		stop("this function requires 'hglm' package to be installed")
@@ -88,12 +138,16 @@
 	out$h2an$estimate <- c(res_hglm$fixef,out$esth2,tVar)
 	names(out$h2an$estimate)[(length(out$h2an$estimate)-1):(length(out$h2an$estimate))] <- 
 			c("h2","totalVar")
+	out$h2an$se <- c(res_hglm$SeFe,NA,NA)
+	names(out$h2an$se) <- 
+			c(names(res_hglm$fixef), "h2","totalVar")
 	out$pgresidualY <- rep(NA,length(mids))
 	out$pgresidualY[mids] <- y-res_hglm$fv
 	out$residualY <- rep(NA,length(mids))
 	out$residualY[mids] <- y - desmat %*% res_hglm$fixef
 	out$InvSigma <- ginv(tVar*out$esth2*relmat + 
 					diag(tVar*(1-out$esth2),ncol=length(y),nrow=length(y)))
+	out$ProfLogLik <- res_hglm$ProfLogLik
 	
 	class(out) <- "polygenic"
 	out
