2d9552a2f68dcd1aa8b73658e173826fbda1b6d5
Yurii Aulchenko <yurii.aulchenko@gmail.com>
2011-05-13 10:17:57 +0000

    Fixing bug [1388], added regression test
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/genabel/pkg/GenABEL@713 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onLoad <- function(lib, pkg) {
 	GenABEL.version <- "1.6-7"
-	cat("GenABEL v.",GenABEL.version,"(May 05, 2011) loaded\n")
+	cat("GenABEL v.",GenABEL.version,"(May 13, 2011) loaded\n")
 	
 	# check for updates and news
 	address <- c(
