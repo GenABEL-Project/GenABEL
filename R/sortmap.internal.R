9bb1f39be28ce379a60fb9d7168c9dce48360b83
Yurii Aulchenko <yurii.aulchenko@gmail.com>
2011-12-05 16:23:11 +0000

    Addressed bug [#1673], added unit test runit.sortmap.internal.R/test.sortmap.internal.bug1673
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/genabel/pkg/GenABEL@813 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/sortmap.internal.R
+++ R/sortmap.internal.R
@@ -1,16 +1,16 @@
 "sortmap.internal" <-
-function(chrom,map,delta=1) {
+		function(chrom,map,delta=1) {
 	chnum <- chrom.char2num(chrom)
-
+	
 	ix <- order(chnum,map)
-
+	
 	map <- map[ix]
-
-   off <- c(0,map[1:(length(map)-1)])
+	
+	off <- c(0,map[1:(length(map)-1)])
 	off <- map - off
 	off[which(off<=0)] <- delta
 	cummap <- cumsum(off)
-
+	
 	out <- list()
 	out$ix <- ix
 	out$cummap <- cummap
@@ -20,11 +20,15 @@ function(chrom,map,delta=1) {
 
 chrom.char2num <- function(chrom) {
 	chrom <- as.character(chrom)
-   chdesU <- unique(chrom)
-   chnumU <- suppressWarnings(as.numeric(chdesU))
-   chCHU <- sort(chdesU[is.na(chnumU)])
-	maxch <- max(chnumU,na.rm=T)
-   j <- 1
+	chdesU <- unique(chrom)
+	chnumU <- suppressWarnings(as.numeric(chdesU))
+	chCHU <- sort(chdesU[is.na(chnumU)])
+	if (any(!is.na(chnumU))) {
+		maxch <- max(chnumU,na.rm=T)
+	} else {
+		maxch <- 0
+	}
+	j <- 1
 	for (i in chCHU) {
 		chrom[which(chrom==i)] <- as.character(maxch+j)
 		j <- j + 1
