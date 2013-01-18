04e81d82c36c0ca49106affadc0027a4a51ca547
Yurii Aulchenko <yurii.aulchenko@gmail.com>
2013-01-18 19:37:08 +0000

    fixing bug in ztransform; version change
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/genabel/pkg/GenABEL@1087 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/zzz.R
+++ R/zzz.R
@@ -4,8 +4,8 @@
 	#pkgDescription <- packageDescription(pkg)
 	#pkgVersion <- pkgDescription$Version
 	#pkgDate <- pkgDescription$Date
-	pkgVersion <- "1.7-3"
-	pkgDate <- "January 09, 2013"
+	pkgVersion <- "1.7-4"
+	pkgDate <- "January 18, 2013"
 	welcomeMessage <- paste(pkg," v. ",pkgVersion," (",pkgDate,") loaded\n",sep="")
 	# check if CRAN version is the same as loaded
 	cranVersion <- try( checkPackageVersionOnCRAN(pkg) )
