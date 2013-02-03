2544f09b87a8d9a83b7a7ac0a26b884a668c6b4d
Yurii Aulchenko <yurii.aulchenko@gmail.com>
2013-02-03 15:39:12 +0000

    bug in 'grammar', smaller default timeout in version checks
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/genabel/pkg/GenABEL@1090 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/zzz.R
+++ R/zzz.R
@@ -5,7 +5,7 @@
 	#pkgVersion <- pkgDescription$Version
 	#pkgDate <- pkgDescription$Date
 	pkgVersion <- "1.7-4"
-	pkgDate <- "January 18, 2013"
+	pkgDate <- "February 03, 2013"
 	welcomeMessage <- paste(pkg," v. ",pkgVersion," (",pkgDate,") loaded\n",sep="")
 	# check if CRAN version is the same as loaded
 	cranVersion <- try( checkPackageVersionOnCRAN(pkg) )
@@ -24,7 +24,7 @@
 			"http://www.genabel.org/sites/default/files/version_and_news.html"
 	)
 	svtmo <- options("timeout")
-	options("timeout"=10)
+	options("timeout"=2)
 	tryRes1 <- 0; class(tryRes1) <- "try-error"
 	curaddr <- 1
 	while (class(tryRes1) == "try-error" && curaddr <= length(address) ) {
