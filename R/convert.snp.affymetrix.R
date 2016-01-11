2a686cc798084a6277bcf887c0a8a98dce0cf8d7
Lennart C. Karssen <lennart@karssen.org>
2016-01-11 20:26:01 +0000

    Summary: Bring GenABEL's convert.snp.affymetrix() up to coding standards
    - Fixed white space
    - fixed use of {}
    - Removed useless lines from the header comments.
    
    
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/genabel/pkg/GenABEL@2035 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/convert.snp.affymetrix.R
+++ R/convert.snp.affymetrix.R
@@ -6,8 +6,6 @@
 #
 #        Version:  1.0
 #        Created:  16-Apr-2008
-#       Revision:  none
-#				last modification: 16-Apr-2008
 #
 #         Author:  Maksim V. Struchalin, Yurii S. Aulchenko
 #        Company:  ErasmusMC, Epidemiology & Biostatistics Department, The Netherlands.
@@ -16,56 +14,45 @@
 #=====================================================================================
 
 
-"convert.snp.affymetrix" <-
-function(dir, map, outfile, skipaffym) {
-
-if (!is(dir,"character"))
-	{
-	stop("Wrong data class: the first argument should be character")
-	}
+"convert.snp.affymetrix" <- function(dir, map, outfile, skipaffym) {
+    if (!is(dir, "character"))
+    {
+        stop("Wrong data class: the first argument should be character")
+    }
 
 
-if (!is(map,"character"))
-	{
-	stop("Wrong data class: the second argument should be character")
-	}
+    if (!is(map, "character"))
+    {
+        stop("Wrong data class: the second argument should be character")
+    }
 
 
-if (!is(outfile,"character"))
-	{
-	stop("Wrong data class: the third argument should be character")
-	}
+    if (!is(outfile, "character"))
+    {
+        stop("Wrong data class: the third argument should be character")
+    }
 
+    command <- paste("ls ", dir, sep="")
+    fileslist <- system(command, intern = TRUE)
+    files_amount <- length(fileslist)
 
+    if (files_amount == 0) {
+        stop("\ndirectory ", dir, " is empty or doesn't exsist\n")
+    }
 
+    alleleID_raw        <- alleleID.char2raw()
+    alleleID_names_char <- names(alleleID_raw)
+    names(alleleID_raw) <- NULL
+    alleleID_amount     <- length(alleleID_raw)
 
-
-
-command <- paste("ls ", dir,sep="")
-fileslist <- system(command, intern = T)
-
-
-files_amount <- length(fileslist)
-if(files_amount == 0) stop("\ndirectory ", dir, " is empty or doesn't exsist\n")
-
-
-
-
-alleleID_raw <- alleleID.char2raw()
-alleleID_names_char <- names(alleleID_raw)
-names(alleleID_raw) <- NULL
-alleleID_amount <- length(alleleID_raw)
-
-
-d <- .C("convert_snp_affymetrix_C", as.character(dir),
-				as.character(fileslist),
-			 	as.integer(files_amount),
-			 	as.character(map),
-			 	as.character(outfile),
-			 	as.integer(skipaffym),
-			 	as.character(alleleID_names_char),
-			 	as.raw(alleleID_raw),
-			 	as.integer(alleleID_amount))
-
-
+    d <- .C("convert_snp_affymetrix_C",
+            as.character(dir),
+            as.character(fileslist),
+            as.integer(files_amount),
+            as.character(map),
+            as.character(outfile),
+            as.integer(skipaffym),
+            as.character(alleleID_names_char),
+            as.raw(alleleID_raw),
+            as.integer(alleleID_amount))
 }
